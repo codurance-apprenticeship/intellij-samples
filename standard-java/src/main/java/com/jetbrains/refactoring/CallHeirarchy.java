be7dece45f6308b817e9f9bdb935694122d63d14
enricjorda <enric.jorda@codurance.com>
2019-08-06 11:46:03 +0200

    call hierarchy

--- standard-java/src/main/java/com/jetbrains/refactoring/CallHeirarchy.java
+++ standard-java/src/main/java/com/jetbrains/refactoring/CallHeirarchy.java
@@ -2,16 +2,9 @@ package com.jetbrains.refactoring;
 
 
 public class CallHeirarchy {
-    private void bar() {
-        baz();
-    }
-
-    private void foo() {
-        bar();
-    }
 
     //Change signature
-    private void baz() {
+    private void allCallsEndHere() {
     }
 
 }
