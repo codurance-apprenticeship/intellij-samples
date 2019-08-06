f782ec822b99c7dba60a7db1e57f7ee8f486eb2d
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2019-08-06 10:54:28 +0100

    :wrench:

--- standard-java/src/main/java/com/jetbrains/refactoring/ExtractDelegate.java
+++ standard-java/src/main/java/com/jetbrains/refactoring/ExtractDelegate.java
@@ -1,24 +1,32 @@
 package com.jetbrains.refactoring;
 
 
+import org.jetbrains.annotations.NotNull;
+
 public class ExtractDelegate {
-    public static void main(String[] args) {
-        String s = "How to extract a delegate: ";
+  public static void main(String[] args) {
+    String s = "How to extract a delegate: ";
 
-        String hotkey = "unknown";
+    String hotKey = "unknown";
 
-        String[] steps = new String[5];
-        steps[0] = "First select a block of code";
-        steps[1] = "Then press " + hotkey;
-        steps[2] = "Give the method a name";
-        steps[3] = "Assign it a visibility";
-        steps[4] = "Apply the refactoring";
+    String[] steps = getSteps(hotKey);
 
-        System.out.println(s);
+    System.out.println(s);
 
 
-        for(String step : steps) {
-            System.out.println(step);
-        }
+    for (String step : steps) {
+      System.out.println(step);
     }
+  }
+
+  @NotNull
+  private static String[] getSteps(String hotkey) {
+    String[] steps = new String[5];
+    steps[0] = "First select a block of code";
+    steps[1] = "Then press " + hotkey;
+    steps[2] = "Give the method a name";
+    steps[3] = "Assign it a visibility";
+    steps[4] = "Apply the refactoring";
+    return steps;
+  }
 }
